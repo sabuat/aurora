98eebe15b42a38287a9979b783d894c591eb9b73
Sabuat <sabuaturbina@yahoo.com>
2024-09-15 00:24:19 -0300

    Changes to be committed: 	modified:   public/index.html 	modified:   src/App.css 	modified:   src/App.js 	new file:   src/Imagens/cart.png 	new file:   src/Imagens/logo-barra.png 	modified:   src/index.css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './Imagens/logo-barra.png';
 import './App.css';
 
 class App extends Component {
@@ -7,14 +7,10 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <img src={logo} alt="logo" />
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
-    );
+    ); 
   }
 }
 
